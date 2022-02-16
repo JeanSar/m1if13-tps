8b913117f0d0028386c0ef68b87889d37a75e0a1
JeanSar <jeansar504@gmail.com>
2022-02-16 16:52:10 +0100

    Ajout du nom de la a doc

--- users/src/main/java/com/users/mif13/UsersApplication.java
+++ users/src/main/java/com/users/mif13/UsersApplication.java
@@ -1,10 +1,16 @@
 package com.users.mif13;
 
 import com.users.mif13.DAO.UserDAO;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UsersApplication extends SpringBootServletInitializer {
@@ -14,6 +20,14 @@ public class UsersApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(UsersApplication.class, args);
+    }
+
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("0.1") String appVersion) {
+        return new OpenAPI()
+                .info(new Info().title("Users API").version(appVersion)
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 
 }
