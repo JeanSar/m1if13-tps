3c3b276aa169464e14cc51a9ddf5f5287e36739c
JeanSar <jeansar504@gmail.com>
2022-03-11 13:31:27 +0100

    Reset user

--- api/routes/User.ts
+++ api/routes/User.ts
@@ -1,21 +1,5 @@
 import {Request, Response, Router} from "express";
 
-interface Position {
-    x: number,
-    y: number
-}
-
-interface Aventurier {
-    image: string,
-    position: Position,
-    ttl: number
-}
-
-interface User {
-    aventurier: Aventurier,
-    isAdmin: boolean
-}
-
 const userRouter = Router();
 const createConroller = (req: Request, res: Response) => {
 
