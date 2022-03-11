06678a8e93c617767eda9adb94e7fd98f1976d59
JeanSar <jeansar504@gmail.com>
2022-03-11 13:10:31 +0100

    Zrr controller ajouté

---
+++ api/routes/ZRR.ts
@@ -0,0 +1,26 @@
+import {Request, Response, Router} from "express";
+import { Position } from "../Types/types";
+
+const zrrRouter = Router();
+
+zrrRouter.post('/create', (req: Request, res: Response) => {
+
+});
+
+zrrRouter.get('/getOne', (req: Request, res: Response) => {
+    
+});
+
+zrrRouter.get('/getAll', (req: Request, res: Response) => {
+    
+});
+
+zrrRouter.put('/update', (req: Request, res: Response) => {
+    
+});
+
+zrrRouter.delete('/delete', (req: Request, res: Response) => {
+    
+});
+
+export default zrrRouter
