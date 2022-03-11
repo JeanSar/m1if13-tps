f0362b700f781520bb678c6e6348bbd21128d84f
Florian <florian.dantier@outlook.fr>
2022-03-11 12:13:11 +0100

    Changement des fichier .html en .ejs pour pouvoir les servir via res.render().

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,28 @@
 import {Response, Request} from "express/ts4.0";
+import path from 'path'
 import express from 'express';
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
 
 app.get("/", (req: Request, res: Response) => {
+    res.send(" worddld !!!!");
+});
+
+app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello worddld !!!!");
 });
 
-app.listen(3000);
+
+// Après toutes nos routes car si on le met avant, ce code sera appelé avant nos routes et on aura donc tout le temps
+// une 404
+app.use((req: Request, res: Response, next) => {
+    res.status(404).render("notfound");
+});
+
+app.listen(3376);
