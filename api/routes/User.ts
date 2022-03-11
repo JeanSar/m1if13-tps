fb1e3eb7da30c20801921b79385ca6876ecbf3c0
Florian <florian.dantier@outlook.fr>
2022-03-11 14:08:11 +0100

    Ajout validation de données

--- api/routes/User.ts
+++ api/routes/User.ts
@@ -1,12 +1,42 @@
 import {Request, Response, Router} from "express";
-import {User, Position, Aventurier} from "../Types/types";
+import {User} from "../Types/types";
+import {body, validationResult} from "express-validator";
 
-
+const users: User[] = [];
+users.push({aventurier: {image: "test", ttl: 0, position: {x: 0, y: 0}}, isAdmin: false});
 const userRouter = Router();
-const createConroller = (req: Request, res: Response) => {
 
+
+const createController = (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+    users.push(req.body);
+    return res.sendStatus(204);
 }
-userRouter.post('/create', createConroller);
+
+userRouter.post('/create',
+    body("aventurier.image").isString(),
+    body("aventurier.ttl").isNumeric(),
+    body("aventurier.position.x").isNumeric(),
+    body("aventurier.position.y").isNumeric(),
+    body("isAdmin").isBoolean(),
+    createController);
+
+const getOneController = (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+    const id: number = req.body.id;
+}
+userRouter.get('/getOne', body("id").isNumeric(), getOneController);
 
 
-export default userRouter
+const getAllController = (req: Request, res: Response) => {
+    res.send(users);
+}
+userRouter.get('/getAll', getAllController);
+
+export default userRouter;
