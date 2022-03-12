1d5edeeae8871b30c1af5daf07e358cc1e49aa09
JeanSar <jeansar504@gmail.com>
2022-03-12 09:00:48 +0100

    add zrr router

--- api/index.ts
+++ api/index.ts
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 app.use('/user', userRouter);
-
+app.use('/zrr', zrrRouter);
 
 
 // Après toutes nos routes car si on le met avant, ce code sera appelé avant nos routes et on aura donc tout le temps
