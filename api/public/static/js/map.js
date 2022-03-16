d2d952989a9bd615126cf39e4728eae03ff5491d
JeanSar <jeansar504@gmail.com>
2022-03-16 12:05:00 +0100

    Sending ZRR to node server

--- api/public/static/js/map.js
+++ api/public/static/js/map.js
@@ -1,8 +1,5 @@
 // initialisation de la map
 let lat = 45.782, lng = 4.8656, zoom = 19;
-const apiPath = 'http://localhost:3376/admin'
-let gameStarted = false;
-
 
 let mymap = L.map('map', {
     center: [lat, lng],
@@ -31,30 +28,28 @@ let pt1 = {lat: 0, lng: 0};
 let pt2 = {lat: 0.1, lng: 0.1};
 
 let onCreateArea = false;
-let fire = false;
+let zrrCreated = false;
 let countClick = 0;
+const baseURL = "http://localhost:3376/admin";
+
 
 let rectangle = undefined;
 
 document.querySelector("#createArea").addEventListener("click", (e) => {
-	e.preventDefault();
 	onCreateArea = true;
 	if(rectangle !== undefined) {
 		mymap.removeLayer(rectangle);
 	}
 });
 
-document.querySelector("#sendTreasure").addEventListener("click", (e) => {
-	e.preventDefault();
-	fire = true;
-});
-
+// TODO : Redimensionner la vue de la map sur le rectangle de bounds
+// TODO : Retirer la zrr du formulaire du cache
 // Clic sur la carte
-mymap.on('click', async e => {
+mymap.on('click', e => {
 	if(onCreateArea) {
 		countClick++;
 		console.log({countClick})
-		if (countClick === 1) {
+		if(countClick === 1) {
 			pt1.lat = e.latlng.lat;
 			pt1.lng = e.latlng.lng;
 			console.log({pt1})
@@ -65,53 +60,10 @@ mymap.on('click', async e => {
 			countClick = 0;
 			onCreateArea = false;
 
-			let bounds = [[pt1.lat, pt1.lng], [e.latlng.lat, e.latlng.lng]];
-			rectangle = L.rectangle(bounds).addTo(mymap);
+			let bounds = [[pt1.lat, pt1.lng], [pt2.lat, pt2.lng]];
+			rectangle = L.rectangle(bounds, {weight: 1}).addTo(mymap);
+			setZRR(rectangle.getBounds());
 		}
-	} else if(fire) { // Si on a presser le bouton fire, un clique sur la map déclenche le pop d'un coffre
-		const {lat: coffre_lat ,lng : coffre_lng} = e.latlng;
-		const composition = document.querySelector("#treasureType").value;
-		const body = {
-			position: {
-				x: coffre_lat,
-				y: coffre_lng
-			},
-			composition: composition
-		}
-		try {
-			const res = await fetch(`${apiPath}/popTresor`, {
-				method: "POST",
-				headers: {'content-type': "application/json"},
-				body: JSON.stringify(body)
-			});
-
-			if(res.status === 201) { // Le trésor a bien été ajouté
-				const coffreIcon = L.icon({
-					iconUrl: "./icon_coffre.png"
-				})
-				// TODO - Centrer le coffre via une fonction, puis récupré la vrai position via l'inverse de cette fonction
-				L.marker([coffre_lat, coffre_lng], {icon: coffreIcon})
-					.addTo(mymap).bindPopup(`Coffre contenant:<br><strong>${composition}}</strong>.`)
-					.openPopup();
-
-				if(gameStarted === false) {
-					const res2 = await fetch(`${apiPath}/startGame`, {
-						method: "POST",
-						headers: {'content-type': "application/json"}
-					});
-
-					console.log(res2)
-					if(res2.status === 204) { // Le serveur a bien commencé partie
-						gameStarted = true;
-					}
-				}
-
-			}
-		} catch (e) {
-			console.error(e.message);
-		}
-
-
 	} else {
 		lat = e.latlng.lat;
 		lng = e.latlng.lng;
@@ -120,6 +72,56 @@ mymap.on('click', async e => {
 	}
 
 });
+
+
+function setZRR(bounds) {
+	let a = bounds.getNorthWest(); //Point a
+	let b = bounds.getSouthEast(); // Point b
+	document.querySelector('#lat1').value = a.lat;
+	document.querySelector('#lon1').value = a.lng;
+	document.querySelector('#lat2').value = b.lat;
+	document.querySelector('#lon2').value = b.lng;
+	zrrCreated = true;
+}
+
+document.querySelector('#submitSendZrr').addEventListener('click', e => {
+	e.preventDefault();
+	sendZRR();
+});
+async function sendZRR() {
+	if(zrrCreated) {
+		const xNO = Number.parseFloat(document.querySelector("#lat1").value);
+		const yNO = Number.parseFloat(document.querySelector("#lon1").value);
+		const xSE = Number.parseFloat(document.querySelector("#lat2").value);
+		const ySE = Number.parseFloat(document.querySelector("#lon2").value);
+		const xNE = xSE;
+		const yNE = yNO;
+		const xSO = xNO;
+		const ySO = ySE;
+	
+		const body = JSON.stringify(
+			{
+				limite_NE: {x: xNE, y: yNE},
+				limite_NO: {x: xNO, y: yNO},
+				limite_SE: {x: xSE, y: ySE},
+				limite_SO: {x: xSO, y: ySO}		
+			}
+		);
+		console.log(body);
+		try {
+		const res = await fetch(`${baseURL}/areaLimit`, {
+			method: "POST",
+			body: body,
+			headers: {'content-type': "application/json"}
+		});
+		console.log('ZRR envoyé.')
+		} catch(e) {
+			console.error(e.message);
+		}
+
+		
+	}
+}
 
 // Mise à jour de la map
 function updateMap() {
