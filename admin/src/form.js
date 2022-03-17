eed20a53ab3b9ac308619b6d565592c29e423c5b
Florian <florian.dantier@outlook.fr>
2022-03-17 13:25:15 +0100

    ajout de quelques gestion de formulaire

--- admin/src/form.js
+++ admin/src/form.js
@@ -2,10 +2,10 @@ import $ from 'jquery';
 
 const apiPath = 'http://localhost:3376/admin'
 // MàJ de l'indicateur numérique du zoom
-function updateZoomValue() {
-    $('#zoomValue').html($('#zoom').val());
-	updateMap();
-}
+// function updateZoomValue() {
+//     $('#zoomValue').html($('#zoom').val());
+// 	updateMap();
+// }
 
 
 // // Abonnement aux événements de changement
@@ -36,8 +36,27 @@ document.querySelector("#okTTL").addEventListener("click", async (e) => {
     }
 });
 
-fetch(`${apiPath}/playerTtl/Toto`).then(res => res.json()).then(resJSON => {
-    const TTLPlayer = document.querySelector("#showPlayerTTL");
-    TTLPlayer.innerHTML = res.aventurier.ttl;
+const addUserIntoZRR = document.querySelector("#addUser");
+// TODO - Message si le joueur est déjà inscrit
+addUserIntoZRR.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const namePlayer = document.querySelector("#namePlayer").value;
+    const body = JSON.stringify({id: namePlayer});
+    try {
+        const res = await fetch(`${apiPath}/registerPlayerZZR`, {
+            method: "POST",
+            body: body,
+            headers: {'content-type': 'application/json'}
+        });
+        if(res.status === 204) {
+            window.alert("Joueur ajouté dans la zrr");
+        } else {
+            window.alert("Aucun joueur de ce nom");
+        }
+    } catch (e) {
+        console.log(e.message);
+    }
 });
+
+
 
