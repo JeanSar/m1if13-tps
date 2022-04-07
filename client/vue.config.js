4f0d57e4eb8124b32a6806849f1cd047f30d8bb6
Florian <florian.dantier@outlook.fr>
2022-04-07 10:14:57 +0200

    Début projet vue : partie client

---
+++ client/vue.config.js
@@ -0,0 +1,7 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    // On se sera amené à utiliser cet objet pour configurer webpack, comme au tp précédent
+  }
+});
