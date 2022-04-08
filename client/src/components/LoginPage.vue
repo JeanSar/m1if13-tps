28f5a414cc641e918b360332e476099ddf1ad1ef
Florian <florian.dantier@outlook.fr>
2022-04-08 10:30:12 +0200

    Enregistrement côté api node lors de la création d'un compte

--- client/src/components/LoginPage.vue
+++ client/src/components/LoginPage.vue
@@ -4,11 +4,17 @@
     <input v-model="loginValue" type="text" name="login" id="login" />
     <br />
     <label for="password">Password :</label>
+    <input v-model="passwordValue" type="password" name="password" id="password" />
+    <br />
+    <label for="image">Image :</label>
+    <input v-model="imageValue" type="url" name="image" id="image" />
+    <br />
+    <label for="ttl">TTL :</label>
     <input
-      v-model="passwordValue"
-      type="password"
-      name="password"
-      id="password"
+      v-model="ttlValue"
+      type="number"
+      name="ttl"
+      id="ttl"
     />
     <br />
     <button @click.prevent="login">Se connecter</button>
@@ -26,6 +32,8 @@ export default {
     return {
       loginValue: "",
       passwordValue: "",
+      imageValue: "",
+      ttlValue: 0
     };
   },
   methods: {
@@ -49,6 +57,25 @@ export default {
     async createAccount() {
       const res = await createAnAccount(this);
       if(res.status === 204) { // La création de compte s'est bien passé
+        fetch('http://localhost:3376/user/create', {
+          method: "POST",
+          body: JSON.stringify({
+            "aventurier": {
+              "id": this.loginValue,
+              "image": this.imageValue,
+              "ttl": this.ttlValue,
+              "position": {
+                "x": 0,
+                "y": 0
+              },
+              "tresors": []
+            },
+            "isAdmin": false
+          }),
+          headers: {
+            "content-type": "application/json"
+          }
+        })
         window.alert("Compte crée ! ");
         // this.loginValue = "";
         // this.passwordValue = "";
