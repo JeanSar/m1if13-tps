7fd3adc03e479ea7761d0b34e436cbbd2efe7e4d
Florian <florian.dantier@outlook.fr>
2022-04-13 09:09:58 +0200

    initialisation store vue x

---
+++ client/src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state() {
+    return {
+      count: 0
+    }
+  },
+  getters: {
+
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
